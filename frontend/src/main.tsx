ae2944827918e6c8c397ba96b6a8fa138503d29f
PedroHR03 <pedrohenriqueroza@gmail.com>
2026-04-03 18:43:20 -0300

    Corrige importação e localização do App.tsx

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,7 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from 'C:/Users/pedro/projetoABP/frontend/src/App'
+import App from './App.tsx'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode><App /></React.StrictMode>,
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
 )
